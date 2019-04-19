7d738409a8c81e771e60e681f82d7dcda8a116cd
luoandy <luo5865264@gmail.com>
2019-04-19 13:27:05 +0800

    like组件

--- components/like/index.js
+++ components/like/index.js
@@ -4,20 +4,39 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    // 开放出來的数据 
+    like: {
+      type: Boolean,
+      value: false,
+      observer: () => {
 
+      },
+    },
+    count: {
+      type: Number,
+      value: 0
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    yesSrc: "./images/like.png",
+    noSrc: "./images/like@dis.png"
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onLike: function (e) {
+      const { like, count } = this.properties
+      const counts = like ? count - 1 : count + 1
+      this.setData({
+        count: counts,
+        like: !like
+      })
+    }
   }
 })
