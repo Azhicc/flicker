84e6ddcd9f29c31928d80a5dfc614a610b019814
高凝露 <gaoninglu@gaoningludeMacBook-Air.local>
2019-04-24 23:07:53 +0800

    首页

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -4,20 +4,38 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    index: Number
+    index: {
+      type: String,
+      observer: function (newVal, oldVal, changedPath) {
+        const val = (newVal < 10) ? '0' + newVal : newVal
+        this.setData({
+          _index: val.toString()
+        })
+      }
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    months: [
+      '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'
+    ],
     year: 0,
-    month: ''
+    month: '',
+    _index: ''
   },
 
   attached: function () {
-    console.log(this.properties)
-    console.log(this.data)
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const { months } = this.data
+    this.setData({
+      year,
+      month: months[month]
+    })
   },
 
   /**
